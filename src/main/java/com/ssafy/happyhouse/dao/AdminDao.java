417717ac9deccbf8985959da1711f6fb6ab192c8
김민지 <39117025+mjkim103301@users.noreply.github.com>
2021-05-07 10:00:40 +0900

    chore: annotation 추가

--- src/main/java/com/ssafy/happyhouse/dao/AdminDao.java
+++ src/main/java/com/ssafy/happyhouse/dao/AdminDao.java
@@ -1,5 +1,8 @@
 package com.ssafy.happyhouse.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface AdminDao {
 
 }
--- src/main/java/com/ssafy/happyhouse/service/AdminServiceImpl.java
+++ src/main/java/com/ssafy/happyhouse/service/AdminServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ssafy.happyhouse.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminServiceImpl implements AdminService {
 
 }
